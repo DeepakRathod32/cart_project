583d264abac34afe5ca7802f588720e252e49f3d
deepakrathod32 <deepakrathod7132@gmail.com>
2022-04-24 19:47:18 +0530

    Updates

---
+++ precache-manifest.593927ad76f0509b3ec65c97f9fdc2ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "713b65d3633a1c4ee17b",
+    "url": "/cart_project/static/css/main.d5341b79.chunk.css"
+  },
+  {
+    "revision": "713b65d3633a1c4ee17b",
+    "url": "/cart_project/static/js/main.713b65d3.chunk.js"
+  },
+  {
+    "revision": "29bafc83cf1d1bba895d",
+    "url": "/cart_project/static/js/1.29bafc83.chunk.js"
+  },
+  {
+    "revision": "574547fa15ec9b0ce65a",
+    "url": "/cart_project/static/js/runtime~main.574547fa.js"
+  },
+  {
+    "revision": "bf7154fc5e3ea9ffdd6c50493a72fc01",
+    "url": "/cart_project/index.html"
+  }
+];
